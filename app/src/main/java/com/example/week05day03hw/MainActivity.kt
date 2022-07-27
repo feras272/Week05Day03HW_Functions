5bb2aa60ca13a80bf93e3e14ddced2b38819c4b0
feras alnowiser <ferasdev@gmail.com>
2022-07-27 19:22:22 +0300

    Initial commit

---
+++ app/src/main/java/com/example/week05day03hw/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.week05day03hw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        
+    }
+
+
+}
