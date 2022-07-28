010058d122bbe398339fd0c9b3b86da043581c03
feras alnowiser <ferasdev@gmail.com>
2022-07-28 16:40:52 +0300

    adding types of functions with button action to activate

--- app/src/main/java/com/example/week05day03hw/MainActivity.kt
+++ app/src/main/java/com/example/week05day03hw/MainActivity.kt
@@ -10,18 +10,41 @@ class MainActivity : AppCompatActivity() {
 
     private var textViewStart:TextView? = null
     private var buttonStart:Button? = null
+    private var price:Double = 100.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         connectViews()
+            printFullName("Feras", "Nasser", "Alnowiser")
+            price.addTax()
 
     }
 
     private fun connectViews() {
         textViewStart = findViewById(R.id.tv_start_activity_main)
         buttonStart = findViewById(R.id.btn_start_activity_main)
+    }
+
+    private fun printFullName(vararg fullName:String) {
+        buttonStart?.setOnClickListener {
+            var yourName:String = ""
+            for(name in fullName) {
+                yourName = yourName + " " + name
+            }
+            Toast.makeText(this, yourName, Toast.LENGTH_SHORT).show()
+            textViewStart?.text = yourName
+        }
+
+    }
+
+    private fun Double.addTax() {
+        val VAT:Double = this * 0.15
+        val finalAmount:Double = VAT + this
+
+        Toast.makeText(this@MainActivity, finalAmount.toString(), Toast.LENGTH_LONG).show()
+
     }
 
 
